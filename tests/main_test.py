f778bb511a23db74e3887cfaac5cebc396cc5b5a
JoanE <joan.espias@gmail.com>
2023-02-28 15:01:23 +0100

    test_five_dice_values rouge

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,9 @@
 import main
 
+
 def test_dice_value():
     assert main.dice_value() in range(1, 7)
+
+
+def test_five_dice_values():
+    assert main.five_dice_values() in [range(1, 7), range(1, 7), range(1, 7), range(1, 7), range(1, 7)]
