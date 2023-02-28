9bd51a5506bf04b281a8ceeecf383ea1dd0b996c
Nicolas Galliere <ngal33@hotmail.fr>
2023-02-28 16:44:02 +0100

    Refacto sum_of_ones et sum_of_twos en sum_of_dices_by_values pour remplir les scores de combinaisons de toutes les valeurs en simultané

--- tests/main_test.py
+++ tests/main_test.py
@@ -18,6 +18,7 @@ def test_sum_of_3_ones():
     # Assert
     assert test_sum_of_three_ones == 3
 
+
 def test_sum_of_2_twos():
     # Arrange
     main.hand = [Dice(2), Dice(2), Dice(1), Dice(3), Dice(5)]
@@ -26,3 +27,13 @@ def test_sum_of_2_twos():
     test_sum_of_two_twos = score_card['twos']
     # Assert
     assert test_sum_of_two_twos == 4
+
+
+def test_chance():
+    # Arrange
+    main.hand = [Dice(1), Dice(2), Dice(1), Dice(3), Dice(5)]
+    # Act
+    main.sum_of_dices_by_value(main.hand)
+    test_of_chance_value = score_card['chance']
+    # Assert
+    assert test_of_chance_value == 12
