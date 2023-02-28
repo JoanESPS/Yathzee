eb9d2bf7f268a2ba2586bc85f76fdac3028c230a
JoanE <joan.espias@gmail.com>
2023-02-28 17:38:02 +0100

    refacto de toutes les fonctions de prod

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ hand = Hand()
 hand.roll_dices()
 
 
-def sum_of_dices_by_value(player_hand):
+def score_card_completion(player_hand):
     score_card = init_score_card()
-    combination_names = list(score_card.keys())
-    for dice in player_hand:
-        score_card[combination_names[dice.value - 1]] += dice.value
-        score_card['chance'] += dice.value
-    return score_card
+    sum_of_dices_values = 0
 
-
-def three_ok_a_kind(player_hand):
-    score_card = init_score_card()
     counter_by_value = [0, 0, 0, 0, 0, 0]
     for dice in player_hand:
         counter_by_value[dice.value - 1] += 1
+        sum_of_dices_values += dice.value
+
+    combination_names = list(score_card.keys())
+    for i in range(6):
+        score_card[combination_names[i]] += counter_by_value[i] * (i+1)
+
+    score_card['chance'] = sum_of_dices_values
+
     if 3 in counter_by_value:
-        score = 0
-        for i in range(1, 7):
-            score += (i * counter_by_value[i-1])
-        score_card["three_of_a_kind"] = score
+        score_card['three_of_a_kind'] = sum_of_dices_values
+
     return score_card
--- tests/main_test.py
+++ tests/main_test.py
@@ -13,7 +13,7 @@ def test_sum_of_3_ones():
     # Arrange
     main.hand = [Dice(1), Dice(1), Dice(1), Dice(3), Dice(5)]
     # Act
-    score_card = main.sum_of_dices_by_value(main.hand)
+    score_card = main.score_card_completion(main.hand)
     test_sum_of_three_ones = score_card['ones']
     # Assert
     assert test_sum_of_three_ones == 3
@@ -23,7 +23,7 @@ def test_sum_of_2_twos():
     # Arrange
     main.hand = [Dice(2), Dice(2), Dice(1), Dice(3), Dice(5)]
     # Act
-    score_card = main.sum_of_dices_by_value(main.hand)
+    score_card = main.score_card_completion(main.hand)
     test_sum_of_two_twos = score_card['twos']
     # Assert
     assert test_sum_of_two_twos == 4
@@ -33,7 +33,7 @@ def test_chance():
     # Arrange
     main.hand = [Dice(1), Dice(2), Dice(1), Dice(4), Dice(5)]
     # Act
-    score_card = main.sum_of_dices_by_value(main.hand)
+    score_card = main.score_card_completion(main.hand)
     test_of_chance_value = score_card['chance']
     # Assert
     assert test_of_chance_value == 13
@@ -43,7 +43,7 @@ def test_three_of_a_kind():
     # Arrange
     main.hand = [Dice(1), Dice(1), Dice(1), Dice(4), Dice(5)]
     # Act
-    score_card = main.three_ok_a_kind(main.hand)
+    score_card = main.score_card_completion(main.hand)
     three_of_a_kind_value = score_card['three_of_a_kind']
     # Assert
     assert three_of_a_kind_value == 12
