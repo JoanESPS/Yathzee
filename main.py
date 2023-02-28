a276b2886f2d76accef68f61729f891f27db0bb7
JoanE <joan.espias@gmail.com>
2023-02-28 17:20:12 +0100

    test_three_ok_a_kind vert

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ def sum_of_dices_by_value(player_hand):
         score_card['chance'] += dice.value
     return score_card
 
+
+def three_ok_a_kind(player_hand):
+    score_card = init_score_card()
+    counter_by_value = [0, 0, 0, 0, 0, 0]
+    for dice in player_hand:
+        counter_by_value[dice.value - 1] += 1
+    if 3 in counter_by_value:
+        score = 0
+        for i in range(1, 7):
+            score += (i * counter_by_value[i-1])
+        score_card["three_of_a_kind"] = score
+    return score_card
