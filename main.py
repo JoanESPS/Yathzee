2b777497e72d266df9fd554dcfebd912ae3814c4
JoanE <joan.espias@gmail.com>
2023-02-28 14:57:35 +0100

    test_dice_value vert

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+
+def dice_value():
+    return random.randint(1, 6)
+
+print(dice_value())
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,4 @@
 import main
 
 def test_dice_value():
-    assert dice_value() in range(1, 7)
+    assert main.dice_value() in range(1, 7)
