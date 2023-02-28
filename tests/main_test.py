03b0054ee3f466aff18d69d45ef275da43b90be0
Nicolas Galliere <ngal33@hotmail.fr>
2023-02-28 16:11:03 +0100

    Ajout test_sum_of_2_twos rouge

--- tests/main_test.py
+++ tests/main_test.py
@@ -15,3 +15,11 @@ def test_sum_of_3_ones():
     test_sum_of_three_ones = main.sum_of_ones(main.hand)
     # Assert
     assert test_sum_of_three_ones == 3
+
+def test_sum_of_2_twos():
+    # Arrange
+    main.hand = [Dice(2), Dice(2), Dice(1), Dice(3), Dice(5)]
+    # Act
+    test_sum_of_two_twos = main.sum_of_twos(main.hand)
+    # Assert
+    assert test_sum_of_three_ones == 4
